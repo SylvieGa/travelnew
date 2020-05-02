cabf457c9b989f88d483cf2be344ad5fcf8c752a
SylvieGa <51321382+SylvieGa@users.noreply.github.com>
2020-05-02 19:12:59 +0200

    Update models.py

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ db = SQLAlchemy(os.getenv("DATABASE_URL",
                           "sqlite:///localhost.sqlite"))  # this connects to a database either on Heroku or on localhost
 
 
-class User(db.Model):
+class user(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True)  # email must be unique! Two users cannot have the same email address
     password = db.Column(db.String)
