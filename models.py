43d6bac48de50d8d46a49d61eb7f15a7985c1304
SylvieGa <51321382+SylvieGa@users.noreply.github.com>
2020-05-03 15:29:21 +0200

    Update models.py

--- models.py
+++ models.py
@@ -7,6 +7,6 @@ db = SQLAlchemy(os.getenv("DATABASE_URL",
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, unique=True)  # email must be unique! Two users cannot have the same email address
-    password = db.Column(db.String)
+    user_name = db.Column(db.String, unique=True)  # email must be unique! Two users cannot have the same email address
+    user_password = db.Column(db.String)
     session_token = db.Column(db.String)
